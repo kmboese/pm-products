e1f8a6ad89b37427d0c6ef843a6b3d66906cdb17
Kevin Boese <kmboese@ucdavis.edu>
2018-09-13 20:18:33 -0700

    Fixed list filtering

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // Import the Product interface
 import { IProduct } from './product';
+import { StarComponent } from '../shared/star.component';
 
 @Component({
   selector: 'pm-products',
@@ -12,13 +13,12 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   // Indicates whether the images in the table are displayed at full resolution
-  showImage: boolean = false;
+  showImage: boolean = true;
   showImageCount: number = 0;
   showImageButtonTitle: string = "Show Image";
 
   errorMessage: string = "Error";
 
-  filteredProducts: IProduct[];
   _listFilter: string;
   get listFilter(): string {
     return this._listFilter;
@@ -28,20 +28,20 @@ export class ProductListComponent implements OnInit {
     this._listFilter = input;
 
     // Save the list of filtered products
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
 
-    console.log(`listFilter changed to ${this._listFilter}`);
+    console.log(`listFilter changed to ${this.listFilter}`);
   }
 
   
-
+  filteredProducts: IProduct[];
   // Hard-code the product data for now
   products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Trebuchet",
       "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
+      "releaseDate": "March 19, 2018",
       "description": "A seige weapon that can launch a 90kg payload over 300 meters.",
       "price": 19.95,
       "starRating": 3.2,
@@ -51,12 +51,28 @@ export class ProductListComponent implements OnInit {
       "productId": 2,
       "productName": "Garden Cart",
       "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
+      "releaseDate": "March 18, 2018",
       "description": "15 gallon capacity rolling garden cart",
       "price": 32.99,
-      "starRating": 4.2,
+      "starRating": 2.1,
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+    },
+    {
+      "productId": 3,
+      "productName": "Electric Guitar",
+      "productCode": "GDN-1157",
+      "releaseDate": "April 15, 2018",
+      "description": "Stringed instrument",
+      "price": 799.99,
+      "starRating": 4.3,
+      "imageUrl": "https://andertons-productimages.imgix.net/88675-tmpD520.jpg?w=1200&h=630&fit=fill&bg=FFFFFF&auto=compress&auto=format"
+    }
+  ];
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this._listFilter = 'cart';
+  }
 
   //OnInit() lifecycle hook startup method
   ngOnInit(): void {
@@ -70,7 +86,7 @@ export class ProductListComponent implements OnInit {
     // Change the button text to say "Hide Image" if the images are displayed, and
     // "Show Image" if the images are not displayed.
     this.showImageButtonTitle = this.showImage ? "Hide Image" : "Show Image";
-    console.log(`ShowImage has been pressed ${++this.showImageCount} times.`);
+    this.showImageCount++;
   }
 
   // Filter the product list based on the search text box string
