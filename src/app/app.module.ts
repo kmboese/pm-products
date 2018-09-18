5e6875dcde1751d24eef5ab2fe80d6222965a995
Kevin Boese <kmboese@ucdavis.edu>
2018-09-17 17:28:30 -0700

    Created product-detail shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   // Local modules
@@ -15,6 +16,7 @@ import { StarComponent } from './shared/star.component';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
   ],
   // External Angular modules
   imports: [
